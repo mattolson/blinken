0569c2119b5d4edd2cf815ad9cb3531de6720cb6
Matt Olson <matt@mattolson.com>
2013-03-04 14:46:15 -0800

    Merge socket and server start
    -- No point in keeping them separate
    -- Make sure they play nice with express

--- driver/index.js
+++ driver/index.js
@@ -1,7 +1,3 @@
 var server = require("./server.js");
-var sockets = require("./sockets.js");
-
-// Start everything up
 server.start();
-sockets.start(server.app);
 
--- driver/server.js
+++ driver/server.js
@@ -1,24 +1,37 @@
 var express = require('express');
+var socket = require('socket.io');
+var http = require('http');
 var handlers = require("./handlers");
 
-var app;
+var app, io;
 
 function start() {
-  // Create express server
+  // Create servers
   app = express();
+  server = http.createServer(app);
 
-  // Serve static files out of '/static' directory
+  // Instantitate web socket
+  io = socket.listen(server);
+  io.set('log level', 1);  
+
+  // Start http server
+  server.listen(8888);
+  console.log("Listening on port 8888...");
+
+  // Configure to serve static files out of '/static' directory
   app.use(express.static('static'));
 
   // Register http handlers
   handlers.registerHttpHandlers(app);
 
-  // Start server
-  app.listen(8888);
-  console.log("Listening on port 8888...");
+  // Register socket handlers
+  io.on("connection", function(socket) {
+    handlers.registerSocketHandlers(socket);
+  });
 
   // Export for later
   exports.app = app;
+  exports.io = io;
 }
 
 exports.start = start;
