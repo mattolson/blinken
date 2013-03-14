07f65a7452a36b7fea1f9099323654b12216e6b9
Matt Olson <matt@mattolson.com>
2013-03-14 14:21:22 -0700

    Only call grid listeners if contents changed

--- driver/controller.js
+++ driver/controller.js
@@ -3,20 +3,24 @@
 function Controller(grid) {
   this.grid = grid;
   this.effects = {};
+  this.num_effects = 0;
   this.rendering = false;
   this.timer = null;
 }
 
 Controller.prototype.register_effect = function(effect) {
   this.effects[effect.name] = effect;
+  this.num_effects++;
 };
 
 Controller.prototype.deregister_effect = function(effect) {
   delete this.effects[effect.name];
+  this.num_effects--;
 };
 
 Controller.prototype.deregister_all = function() {
   this.effects = {};
+  this.num_effects = 0;
 };
 
 Controller.prototype.run = function() {
@@ -41,6 +45,9 @@ Controller.prototype.render = function() {
   // Global lock to make sure this doesn't get called again until we're done
   this.rendering = true;
 
+  // The grid will have changed if there is at least one effect in our list
+  var changed = this.num_effects > 0;
+
   // Loop through effects and have them render themselves
   for (var effect in this.effects) {
     if (!effect.render()) {
@@ -50,7 +57,9 @@ Controller.prototype.render = function() {
   }
 
   // Blast updates to strip
-  this.grid.sync();
+  if (changed) {
+    this.grid.sync();
+  }
 
   // Remove lock
   this.rendering = false;
