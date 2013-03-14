27408ac572e4c0876008df9d4039c1aed13bd532
Matt Olson <matt@mattolson.com>
2013-03-14 13:03:34 -0700

    Fix effect inheritance

--- driver/effects/throb.js
+++ driver/effects/throb.js
@@ -9,7 +9,7 @@ var STEPS = 25;
 // options = {}, optional, valid keys: 'easing' => type of easing to perform (defaults to 'linear')
 function Throb(grid, period, start_color, end_color, options)
 {
-  this.super_.call(this, grid, period);
+  Throb.super_.call(this, grid, period);
   this.start_color = start_color;
   this.end_color = end_color;
   this.step = 0;
