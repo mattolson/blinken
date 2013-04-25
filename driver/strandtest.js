309e417a2ba83754dc41174ed63cb6b11cdcb383
SEan <hi.na3s@gmail.com>
2013-04-24 22:30:09 -0700

    Revert wouldn't work right
    (shouldn't be conflicts, won't happen again)

--- driver/strandtest.js
+++ driver/strandtest.js
@@ -1,6 +1,17 @@
+// Get number of panels
+var num_panels = process.argv[2];
+if (typeof(num_panels) === 'undefined') {
+  console.log('USAGE: sudo node strandtest.js <num_panels>');
+}
+
 // Setup strand
 var Strand = require('./strand');
-var strand = new Strand('/dev/spidev0.0', 72);
+var strand = new Strand('/dev/spidev0.0', num_panels*18);
 
-strand.setStrandColor(255,255,255);
-strand.sync();
+strand.off();
+
+setTimeout(function() {
+  strand.setStrandColor([255,255,255]);
+  strand.sync();
+}, 1000);
+
