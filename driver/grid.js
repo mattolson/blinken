e87d27a4138bc6051634860746fb855f0fed27d0
SEan <hi.na3s@gmail.com>
2013-04-29 23:38:34 -0700

    Removed hsv to rgb

--- driver/grid.js
+++ driver/grid.js
@@ -220,27 +220,6 @@ Grid.prototype.addListener = function(listener) {
   }
 };
 
-Grid.prototype.HSVtoRGB = function(h, s, v){
-	var r, g, b;
-  var i = Math.floor(h * 6);
-  var f = h * 6 - i;
-  var p = v * (1 - s);
-  var q = v * (1 - f * s);
-  var t = v * (1 - (1 - f) * s);
-
-  switch(i % 6){
-  case 0: r = v, g = t, b = p; break;
-  case 1: r = q, g = v, b = p; break;
-  case 2: r = p, g = v, b = t; break;
-  case 3: r = p, g = q, b = v; break;
-  case 4: r = t, g = p, b = v; break;
-  case 5: r = v, g = p, b = q; break;
-  }
-  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
-}
-
-
-
 //-FIND INDEX OF HORIZONAL OPPOSITE LED
 Grid.prototype.horizontalIndex = function(i) {
   //-ONLY WORKS WITH INDEX < TOPINDEX
