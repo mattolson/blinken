ab02ff6af0e22f43557b7435d209ba0a3e770792
Sandwich <hi.na3s@gmail.com>
2014-11-05 13:53:38 -0800

    Abstracted Registry
    Use like: new Registry( ‘ sources ’ );

---
+++ driver/registry.js
@@ -0,0 +1,44 @@
+var path = require('path');
+
+// Keep track of all sources available in the system, for easy
+// lookup and instantiation.
+function Registry( type ) {
+
+  if(type == 'type') return;
+ 
+  this.type = type;
+  this[this.type] = {};
+
+
+
+   var registry = this;
+  // Loop through directories
+  
+  var dir = path.resolve(__dirname, this.type);
+  require("fs").readdirSync(dir).forEach(function(file) {
+    var module = require(dir + "/" + file);
+    registry[registry.type][module.name] = module.constructor;
+    // console.log(registry[registry.type][module.name]);
+  });
+
+  console.log(this[this.type])
+
+}
+
+Registry.prototype.find = function(name) {
+  return this[this.type][name] || null;
+};
+
+Registry.prototype.toJson = function( what ) {
+  var json = [];
+  for (var name in this[this.type]) {
+    json.push({
+      'name': name,
+      'options': this[this.type][name].options_spec()
+    });
+  }
+  return json;
+};
+
+// Expose singleton
+module.exports = Registry;
