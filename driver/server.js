fa074f3f3db348fd091f702ec1ec62417e5a5dcc
Michael Favor <michaelf@favorsoftware.com>
2015-01-01 20:27:56 -0800

    moved code from index.js to server.js to keep index.js as minimal as possible

--- driver/server.js
+++ driver/server.js
@@ -1,3 +1,11 @@
+// Make sure we have a good config file.
+try {        
+    var Config = require.resolve("./config.js");
+} catch(e) {
+    console.error("Config file is not setup. Please rename config-default.js to config.js");
+    process.exit(e.code);
+}
+
 var express = require('express');
 
 var app = express();
