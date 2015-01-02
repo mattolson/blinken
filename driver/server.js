ddf7a58aa4b5598f1a7a2ef4646d5e69ea9255c4
Michael Favor <michaelf@favorsoftware.com>
2015-01-01 20:20:18 -0800

    changed server to listen on all IP addresses

--- driver/server.js
+++ driver/server.js
@@ -9,9 +9,13 @@ var http = require('http'),
 //Websockets
 io = require('socket.io').listen(server);
 //Websocket streams
+console.log("Loading socket.io-stream");
 require('socket.io-stream')(io);
 
+console.log("Loading API");
 var api = require("./api");
+
+console.log("Loading Config");
 var Config = require("./config");
 var bodyParser = require('body-parser');
 
@@ -24,9 +28,11 @@ function start() {
   app.use(bodyParser.json());  
 
   // Register http handlers
+  console.log("Registering HTTP handlers");
   api.registerHttpHandlers(app);
   
   // Register socket handlers
+  console.log("Registering socket handlers");
   api.registerSocketHandlers();
  
   // Export for later
@@ -34,11 +40,10 @@ function start() {
   exports.io = io;
     
   // Start http server
-  server.listen(Config.server.port, Config.server.host, function ()  {
-  // app.listen(Config.server.port, '192.168.1.6', function ()  {
-    console.log('Listening on '+Config.server.host+':'+Config.server.port);
+  //server.listen(Config.server.port, Config.server.host, function ()  {
+  server.listen(Config.server.port, function ()  {  // listens on all ip addresses if host is not specified    
+    console.log('\nListening on all IP addresses, on port: '+Config.server.port);
   });
-
 }
 
 exports.start = start;
