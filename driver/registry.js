1108a5ac879783c026d260ca6a0bee30f5f98445
Sandwich <hi.na3s@gmail.com>
2015-04-11 00:39:32 -0400

    Housecleaning

--- driver/registry.js
+++ driver/registry.js
@@ -3,26 +3,18 @@ var path = require('path');
 // Keep track of all sources available in the system, for easy
 // lookup and instantiation.
 function Registry( type ) {
-
   if(type == 'type') return;
- 
   this.type = type;
   this[this.type] = {};
-
-
-
    var registry = this;
-  // Loop through directories
   
+  // Loop through directories
   var dir = path.resolve(__dirname, this.type);
   require("fs").readdirSync(dir).forEach(function(file) {
     if(file == '.DS_Store') return;
     var module = require(dir + "/" + file);
     registry[registry.type][module.name] = module.constructor;
-    // console.log(registry[registry.type][module.name]);
   });
-
-  // console.log(this[this.type])
 }
 
 Registry.prototype.find = function(name) {
